e24d4900f648f6b2db3410b4a36d71108270f35f
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-11 15:30:55 -0500

    Add CORS '/*' for origin '0.0.0.0'

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,15 @@
 """
 Starts a Flask web application with blueprint
 """
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, origins="0.0.0.0")
 
 
 @app.errorhandler(404)
