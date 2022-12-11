c0e72246233c6845b85acfd1283f466a84bb2128
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-10 19:18:16 -0500

    Fix pep8 validation in app.py and index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,9 +21,9 @@ if __name__ == "__main__":
     ip = getenv("HBNB_API_HOST")
     port = getenv("HBNB_API_PORT")
 
-    if ip is None: 
+    if ip is None:
         ip = '0.0.0.0'
-    if port is None: 
+    if port is None:
         port = '5000'
 
     app.run(host=ip, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ Create routes:
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route("/status")
 def show_status():
     """Returns the status of the HTTP request"""
