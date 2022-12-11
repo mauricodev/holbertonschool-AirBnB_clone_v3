25dc42f5ad395ea8d729a4488ba6cd2e3bc4bb5d
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-10 19:11:14 -0500

    Create first endpoint of API. This endpoint returns the status in JSON

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 
 if __name__ == "__main__":
-    pass
+    ip = getenv("HBNB_API_HOST")
+    port = getenv("HBNB_API_PORT")
+
+    if ip is None: 
+        ip = '0.0.0.0'
+    if port is None: 
+        port = '5000'
+
+    app.run(host=ip, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route("/status")
+def show_status():
+    """Returns the status of the HTTP request"""
+    res = {
+        "status": "OK"
+    }
+    return (jsonify(res))
