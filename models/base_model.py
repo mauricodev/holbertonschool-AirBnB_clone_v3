bf1c9e8b125d6acb3de945710d4bd153d150fb86
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-12-16 02:02:51 -0500

    Change delete to .pop() in to_dict() to manage default value

--- models/base_model.py
+++ models/base_model.py
@@ -67,9 +67,9 @@ class BaseModel:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
+            new_dict.pop("_sa_instance_state", None)
         if not password:
-            del new_dict["password"]
+            new_dict.pop("password", None)
         return new_dict
 
     def delete(self):
